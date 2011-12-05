6db97670d18c599b4a6587662caba780d6166ad2
Carl Vanderlip <carlv@codeaurora.org>
2011-12-05 12:37:59 -0800

    video: msm: Adding CSC support for other MDP blocks
    Adding support for VG1, VG2, DMA_S and Overlay Mixer 1.
    
    Change-Id: I95a5832a70fd893970e82dadcec1e11ed608d79c
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>
    (cherry picked from commit fa1de67dbadc084aa9fd6afa5ab4fae335089334)

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -704,6 +704,8 @@ void mdp4_set_limit_range(bool set_limit_range);
 void mdp4_hsic_set(struct mdp4_overlay_pipe *pipe, struct dpp_ctrl *ctrl);
 void mdp4_hsic_update(struct mdp4_overlay_pipe *pipe);
 int mdp4_csc_config(struct mdp_csc_cfg_data *config);
+void mdp4_csc_write(struct mdp_csc_cfg *data, uint32_t base);
+int mdp4_csc_enable(struct mdp_csc_cfg_data *config);
 
 u32  mdp4_allocate_writeback_buf(struct msm_fb_data_type *mfd, u32 mix_num);
 void mdp4_init_writeback_buf(struct msm_fb_data_type *mfd, u32 mix_num);
