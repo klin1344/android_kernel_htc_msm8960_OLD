989f1e423475a7c35fa08b9773b6b355ffda451a
Carl Vanderlip <carlv@codeaurora.org>
2012-02-20 19:24:56 -0800

    video:msm: Add support for updating Histogram LUT using MDP_PP ioctl
    This adds the ability to write to the Histogram LUTs in the VG pipes, DMA_P
    and DMA_S.
    
    Change-Id: I548140d01aee5c2fd437aa218a307b4d1364dade
    Signed-off-by: Carl Vanderlip <carlv@codeaurora.org>
    (cherry picked from commit 330b4ae84d423bee684af24a2e52b4d81831a37f)
    
    Conflicts:
    	drivers/video/msm/mdp.h

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -701,6 +701,9 @@ int mdp4_writeback_init(struct fb_info *info);
 int mdp4_writeback_terminate(struct fb_info *info);
 void mdp4_set_limit_range(bool set_limit_range);
 
+uint32_t mdp_block2base(uint32_t block);
+int mdp_hist_lut_config(struct mdp_hist_lut_data *data);
+
 void mdp4_hsic_set(struct mdp4_overlay_pipe *pipe, struct dpp_ctrl *ctrl);
 void mdp4_hsic_update(struct mdp4_overlay_pipe *pipe);
 int mdp4_csc_config(struct mdp_csc_cfg_data *config);
