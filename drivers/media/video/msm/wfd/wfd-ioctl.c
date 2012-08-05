AliceOps/android_kernel_samsung_msm8660-common

msm: vidc: Handling secure/non-secure concurrency.
Change-Id: Ief0e56615872b109b9d56fc55d2dc28707c90862
Signed-off-by: Vinay Kalia <vkalia@codeaurora.org>

Signed-off-by: Vinay Kalia <vkalia@codeaurora.org>

drivers/media/video/msm/wfd/wfd-ioctl.c
@@ -1067,6 +1067,7 @@ static int wfd_open(struct file *filp)
 	enc_mops.op_buffer_done = venc_op_buffer_done;
 	enc_mops.ip_buffer_done = venc_ip_buffer_done;
 	enc_mops.cbdata = filp;
+	enc_mops.secure = wfd_dev->secure_device;
 	rc = v4l2_subdev_call(&wfd_dev->enc_sdev, core, ioctl, OPEN,
 				(void *)&enc_mops);
 	if (rc || !enc_mops.cookie) {

klin1344/android_kernel_htc_msm8960_OLD

Conflicts:
	drivers/media/video/msm/wfd/wfd-ioctl.c
	drivers/video/msm/vidc/common/dec/vdec.c

drivers/media/video/msm/wfd/wfd-ioctl.c
@@ -804,6 +804,7 @@ static int wfd_open(struct file *filp)
 	vmops.op_buffer_done = venc_op_buffer_done;
 	vmops.ip_buffer_done = venc_ip_buffer_done;
 	vmops.cbdata = filp;
+	vmops.secure = wfd_dev->secure_device;
 	rc = v4l2_subdev_call(&wfd_dev->enc_sdev, core, ioctl, OPEN,
 				(void *)&vmops);
 	if (rc || !vmops.cookie) {