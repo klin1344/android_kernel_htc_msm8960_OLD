nobodyAtall/msm7x30-3.0.x-nAa

msm: vidc: Adds API to request performance level.
Adds API to request for a particular performance
level from video core. This is needed for Wi-fi
display (WFD) to reduce end to end latency.

Change-Id: Ib4133b8cd62ccad286be1984f944911754b887c5
Signed-off-by: Vinay Kalia <vkalia@codeaurora.org>
Signed-off-by: Zhoulu Luo <zluo@codeaurora.org>

include/media/msm/vcd_property.h
@@ -51,7 +51,8 @@
 #define VCD_I_DISABLE_DMX (VCD_START_BASE + 0x23)
 #define VCD_I_DISABLE_DMX_SUPPORT (VCD_START_BASE + 0x24)
 #define VCD_I_ENABLE_SPS_PPS_FOR_IDR (VCD_START_BASE + 0x25)
-#define VCD_I_VOP_TIMING_CONSTANT_DELTA (VCD_START_BASE + 0x26)
+#define VCD_REQ_PERF_LEVEL (VCD_START_BASE + 0x26)
+#define VCD_I_VOP_TIMING_CONSTANT_DELTA (VCD_START_BASE + 0x27)
 
 #define VCD_START_REQ      (VCD_START_BASE + 0x1000)
 #define VCD_I_REQ_IFRAME   (VCD_START_REQ + 0x1)
@@ -93,6 +94,11 @@ struct vcd_property_frame_size {
 	u32              scan_lines;
 };
 
+enum vcd_perf_level {
+	VCD_PERF_LEVEL0,
+	VCD_PERF_LEVEL1,
+	VCD_PERF_LEVEL2,
+};
 
 #define VCD_METADATA_DATANONE       0x001
 #define VCD_METADATA_QCOMFILLER     0x002
@@ -122,6 +128,10 @@ struct vcd_property_frame_rate {
 
 struct vcd_property_target_bitrate {
 	u32             target_bitrate;
+};
+
+struct vcd_property_perf_level {
+	enum vcd_perf_level level;
 };
 
 enum vcd_yuv_buffer_format {

klin1344/android_kernel_htc_msm8960_OLD

Conflicts:
	drivers/media/video/msm/wfd/enc-subdev.c
	include/linux/videodev2.h

include/media/msm/vcd_property.h
@@ -51,8 +51,9 @@
 #define VCD_I_DISABLE_DMX (VCD_START_BASE + 0x23)
 #define VCD_I_DISABLE_DMX_SUPPORT (VCD_START_BASE + 0x24)
 #define VCD_I_ENABLE_SPS_PPS_FOR_IDR (VCD_START_BASE + 0x25)
-#define VCD_I_VOP_TIMING_CONSTANT_DELTA (VCD_START_BASE + 0x26)
+#define VCD_REQ_PERF_LEVEL (VCD_START_BASE + 0x26)
 #define VCD_I_SLICE_DELIVERY_MODE (VCD_START_BASE + 0x27)
+#define VCD_I_VOP_TIMING_CONSTANT_DELTA (VCD_START_BASE + 0x28)
 
 #define VCD_START_REQ      (VCD_START_BASE + 0x1000)
 #define VCD_I_REQ_IFRAME   (VCD_START_REQ + 0x1)
@@ -94,6 +95,11 @@ struct vcd_property_frame_size {
 	u32              scan_lines;
 };
 
+enum vcd_perf_level {
+	VCD_PERF_LEVEL0,
+	VCD_PERF_LEVEL1,
+	VCD_PERF_LEVEL2,
+};
 
 #define VCD_METADATA_DATANONE       0x001
 #define VCD_METADATA_QCOMFILLER     0x002
@@ -123,6 +129,10 @@ struct vcd_property_frame_rate {
 
 struct vcd_property_target_bitrate {
 	u32             target_bitrate;
+};
+
+struct vcd_property_perf_level {
+	enum vcd_perf_level level;
 };
 
 enum vcd_yuv_buffer_format {