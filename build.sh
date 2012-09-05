a072c70d32f7c6363d9999999ad4206e5b4ad455
klin1344 <kevlin97@gmail.com>
2012-09-05 13:37:27 -0700

    Bump version number to v2.0 and change kernel name

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 CROSS_COMPILE=/home/klin1344/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
 INITRAMFS_DIR=ramdisk.gz
-KERNEL_NAME=Fusion_Ult_CM10
-KERNEL_VNUMBER=1.0
+KERNEL_NAME=Fusion_Ult_AOSP
+KERNEL_VNUMBER=2.0
 
 # DO NOT MODIFY BELOW THIS LINE
 CURRENT_DIR=`pwd`
